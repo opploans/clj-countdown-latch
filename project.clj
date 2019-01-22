f5352e198725f43cec9b5300c928309983e9da6c
sstarkey <sstarkey@opploans.com>
2019-01-22 11:51:10 -0600

    Put in the opploans group

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-countdown-latch "0.1.0"
+(defproject opploans/clj-countdown-latch "0.1.0"
   :description "Execute code in parallel and wait for all executions to finish"
   :url "https://github.com/opploans/clj-countdown-latch"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
